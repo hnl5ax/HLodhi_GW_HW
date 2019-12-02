cdc98ede4c1c97e405ea77e6b59bab0e52af3905
Hussain <hnl5ax@virginia.edu>
2019-12-02 00:11:18 -0500

    adding sql part 1

---
+++ sql-challenge/Part1/part1.sql
@@ -0,0 +1,70 @@
+-- Create Employees Table
+CREATE TABLE employees
+(
+	emp_no INT PRIMARY KEY NOT NULL,
+	birth_date VARCHAR(30) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	gender VARCHAR(1) NOT NULL,
+	hire_date VARCHAR(30) NOT NULL
+);
+Select * From employees
+-- Create Departments Table
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments
+(
+	dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
+);
+Select * From departments
+-- Create Department Employees Table
+DROP TABLE IF EXISTS dept_emp;
+CREATE TABLE dept_emp
+(
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+Select * From dept_emp
+-- Create Department Managers Table
+DROP TABLE IF EXISTS dept_manager;
+CREATE TABLE dept_manager
+(
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+Select * From dept_manager
+-- Create Salaries Table
+DROP TABLE IF EXISTS salaries;
+CREATE TABLE salaries
+(
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	salary INT NOT NULL,
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+Select * From salaries
+-- Create Titles Table
+DROP TABLE IF EXISTS titles;
+CREATE TABLE titles
+(
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	title VARCHAR(30) NOT NULL,
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+Select * From titles
+-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM salaries AS s
+INNER JOIN employees AS e ON
+e.emp_no = s.emp_no;
